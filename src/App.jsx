e006775c169dadfcec2ce3796e9b822b747a7e33
Mickeyyqt <161735701+Mickeyyqt@users.noreply.github.com>
2024-05-24 10:33:59 +0630

    useReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import Child1 from "./components/Child1";
-import Child2 from "./components/Child2";
-import Child3 from "./components/Child3";
-import FirstContextProvider from "./components/FirstContext";
-
+import React, { useReducer } from "react";
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case "minus":
+      return { ...state, count: state.count - 1 };
+    case "plus":
+      return { ...state, count: state.count + 1 };
+    case "updateKey":
+      return { ...state, key: action.payload };
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(firstReducer, { count: 0, key: "" });
   return (
-    <section>
-      <div>
-        <FirstContextProvider>
-        <Child1 />
-        <Child2 />
-        <Child3 />
-        </FirstContextProvider>
-       
-      </div>
-    </section>
+    <div className="App">
+      <h1>your key is -{state.key}</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          dispatch({ type: "updateKey", payload: e.target.value });
+        }}
+      />
+      <button onClick={() => dispatch({ type: "minus" })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: "plus" })}>+</button>
+    </div>
   );
 }
+
 export default App;
